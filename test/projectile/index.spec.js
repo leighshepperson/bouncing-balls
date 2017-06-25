a07396aa116cda9a2f9fe8bea448c57a50a93ca6
Leigh Shepperson <lshepperson@findmypast.com>
2017-06-25 14:48:08 +0100

    First failing test for getX

--- test/projectile/index.spec.js
+++ test/projectile/index.spec.js
@@ -1,7 +1,17 @@
 import {
   expect
 } from 'chai';
+import {
+  getX
+} from '../../src/projectile';
 
 describe('Projectile', function() {
+  describe('getX', function() {
+    it('computes the x component from velocity and angle', function() {
+      const angle = Math.PI / 3,
+        velocity = 3;
 
+      expect(getX(angle, velocity)).to.be.equal(1.5);
+    });
+  })
 });
