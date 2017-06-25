269a917d45c4c132d047159bb80bdcb5b304d245
Leigh Shepperson <lshepperson@findmypast.com>
2017-06-25 16:51:23 +0100

    Added failing test to add a ball to list of balls

---
+++ test/bouncing-ball-app/index.spec.js
@@ -0,0 +1,32 @@
+import {
+  expect
+} from 'chai';
+import BouncingBallApp from '../../src/bouncing-ball-app';
+
+describe('BouncingBallApp', function() {
+  let contextStub, canvasStub;
+
+  before(function() {
+    contextStub = {};
+    canvasStub = {
+      height: 500,
+      width: 800,
+      offsetLeft: 20,
+      offsetTop: 32
+    };
+  });
+
+  describe('addBall', function() {
+    let bouncingBallApp;
+
+    beforeEach(function() {
+      bouncingBallApp = new BouncingBallApp(canvasStub, contextStub);
+    });
+
+    it('adds a ball to the list of balls', function() {
+      bouncingBallApp.addBall();
+      console.log(bouncingBallApp);
+      expect(bouncingBallApp.balls.length).to.be.equal(1);
+    });
+  });
+});
