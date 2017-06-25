690edda113aed92c0ab2b73b88586a1516073573
Leigh Shepperson <lshepperson@findmypast.com>
2017-06-25 16:15:51 +0100

    Added implementation for update function

--- src/ball/index.js
+++ src/ball/index.js
@@ -1,17 +1,21 @@
 export default class Ball {
-  constructor(context, x, y, vx,vy) {
+  constructor(context, x, y, vx, vy) {
     this.context = context;
-    this.x = x;
-    this.y = y;
-    this.vx = vx;
-    this.vy = vy;
+    this._x = x;
+    this._y = y;
+    this._vx = vx;
+    this._vy = vy;
+  }
+
+  get x() {
+    return this._x;
   }
 
   draw() {
     this.context.beginPath();
     this.context.arc(
-      this.x,
-      this.y,
+      this._x,
+      this._y,
       2,
       0,
       Math.PI * 2,
@@ -19,5 +23,9 @@ export default class Ball {
     );
     this.context.closePath();
     this.context.fill();
+  }
+
+  update() {
+    this._x += this._vx;
   }
 }
